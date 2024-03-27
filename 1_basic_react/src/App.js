e5f2320f83a571b9b38cdd6f069317732d374b3f
Abhishek Surse <abhisheksurse5121999.com>
2024-03-27 23:28:42 +0530

    start with my first react project

---
+++ 1_basic_react/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Hello Abhishek</h1>
+  );
+}
+
+export default App;
---
+++ 1vite_react/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Hello abhishek, i'm reactvite</h1>
+  )
+}
+
+export default App
