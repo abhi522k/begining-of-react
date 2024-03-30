fe548c11bd994af496f3c4ec7ba50dbb937ce34a
Abhishek Surse <abhisheksurse5121999.com>
2024-03-30 17:54:55 +0530

    learn about how to create react-create element.

--- 1vite_react/src/main.jsx
+++ 1vite_react/src/main.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit google</a>
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'visit google'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  //  <App />
+  // anotherElement
 )
