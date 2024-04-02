d59e4fbc917584e3bc389c69e996fdcadcc312df
Abhishek Surse <abhisheksurse5121999.com>
2024-04-02 20:36:38 +0530

    project completed here.

--- 3tailwindprops/src/App.jsx
+++ 3tailwindprops/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './component/card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let obj = {
+    name: 'abhishek',
+    role: 'employee'
+  }
 
   return (
     <>
       <h1 className='bg-green-400 p-4 rounded-xl mb-4'>Tailwind classes</h1>
-      
+      <Card username="Abhishek" someObj={obj} btnText="click me" />
+      <Card username="Abhishek" />
     </>
   )
 }
